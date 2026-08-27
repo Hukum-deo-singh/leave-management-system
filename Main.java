7b7ffef0aca0083573b01ed309ed607ccecdedc4
Hukum Deo Singh <deosinghhukum2@gmail.com>
2026-08-27 22:05:54 +0530

    Update Main.java fix the valuable small bugs nextInt() of java by adding nextline() after nextInt().

--- Main.java
+++ Main.java
@@ -1,16 +1,23 @@
-import java.util.Scanner
+import java.util.Scanner;
 
 class Main {
-public static void main (String[] args)
-{
-Scanner sc = new Scanner();
-System.out.println("----welcome to the leave management system ----");
-System.out.println("let's start managing leaves don't matter they are students, employees and worker !!");
-System.out.println("Enter your company/institution name: ");
-String companyName = sc.nextLine();
-System.out.println("Enter the no. of employees in your company/institution: ");
-int employeesCount = sc.nextInt();
-System.out.println("How you want to manage the leave ('7 days or 1 month')");
-manageLeave = sc.nextLine();
-}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("---- Welcome to the Leave Management System ----");
+        System.out.println("Let's start managing leaves, whether they are students, employees, or workers!");
+
+        System.out.println("Enter your company/institution name: ");
+        String companyName = sc.nextLine();
+
+        System.out.println("Enter the no. of employees in your company/institution: ");
+        int employeesCount = sc.nextInt();
+
+        sc.nextLine(); // Consume leftover newline
+
+        System.out.println("How do you want to manage the leave ('7 days' or '1 month')?");
+        String manageLeave = sc.nextLine();
+
+        sc.close();
+    }
 }
