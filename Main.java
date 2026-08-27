89f7ed2b956b7d56c332b6c6c10d91b910c7e807
Hukum Deo Singh <deosinghhukum2@gmail.com>
2026-08-27 21:52:22 +0530

    Create Main.java with initial overview of my leave management system.

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+public static void main (String[] args)
+{
+System.out.println("----welcome to the leave management system ----");
+System.out.println("let's start managing leaves don't matter they are students, employees and worker !!");
+System.out.println("Enter your company/institution name: ");
+String companyName = sc.nextLine();
+System.out.println("Enter the no. of employees in your company/institution: ");
+int employeesCount = sc.nextInt();
+System.out.println("How you want to manage the leave ('7 days or 1 month')");
+manageLeave = sc.nextLine();
+}
+}
